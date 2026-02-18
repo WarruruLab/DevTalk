5af01e3d3e0d322b5c39e9660104fa37f2d72a3c
pswaao88 <pswaao88@gmail.com>
2026-02-18 18:58:18 +0900

    feat: 특정 id 메세지 리턴 매퍼 작성

--- backend/src/main/java/com/devtalk/devtalk/api/controller/devtalk/internal/InternalController.java
+++ backend/src/main/java/com/devtalk/devtalk/api/controller/devtalk/internal/InternalController.java
@@ -1,11 +1,14 @@
 package com.devtalk.devtalk.api.controller.devtalk.internal;
 
+import com.devtalk.devtalk.api.dto.request.InternalBatchMessageRequest;
 import com.devtalk.devtalk.api.dto.response.InternalMessagePageResponse;
+import com.devtalk.devtalk.api.dto.response.InternalMessageResponse;
 import com.devtalk.devtalk.service.message.MessageService;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,4 +28,8 @@ public class InternalController {
         return ResponseEntity.ok(messageService.getAllMessageByCursor(sessionId, cursor, limit));
     }
 
+    @PostMapping("/messages/batch")
+    public ResponseEntity<List<InternalMessageResponse>> getMessagesBatch(@RequestBody InternalBatchMessageRequest request){
+        return ResponseEntity.ok(messageService.getMessagesByIds(request));
+    }
 }
