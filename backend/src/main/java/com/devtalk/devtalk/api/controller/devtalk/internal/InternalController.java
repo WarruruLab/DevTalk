93861c03029c7025716206b867438805a9af4138
pswaao88 <pswaao88@gmail.com>
2026-02-18 17:52:17 +0900

    feat: 개발로그용 internal api controller 추가
    커서(시간)이후의 메세지를 가져옴

---
+++ backend/src/main/java/com/devtalk/devtalk/api/controller/devtalk/internal/InternalController.java
@@ -0,0 +1,28 @@
+package com.devtalk.devtalk.api.controller.devtalk.internal;
+
+import com.devtalk.devtalk.api.dto.response.InternalMessagePageResponse;
+import com.devtalk.devtalk.service.message.MessageService;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/internal")
+public class InternalController {
+    private final MessageService messageService;
+
+    public InternalController(MessageService messageService){
+        this.messageService = messageService;
+    }
+
+    @GetMapping("/messages/{sessionId}")
+    public ResponseEntity<InternalMessagePageResponse> getMessages(@PathVariable String sessionId, @RequestParam(name = "cursor", required = false) String cursor, @RequestParam(name = "limit", defaultValue = "100") String limit){
+        return ResponseEntity.ok(messageService.getAllMessageByCursor(sessionId, cursor, limit));
+    }
+
+}
